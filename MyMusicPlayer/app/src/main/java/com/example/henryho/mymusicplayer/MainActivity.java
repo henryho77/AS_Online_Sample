d90ae96a75fc954a115f5f9f9792f49963276325
henryho77 <squirrelho@gmail.com>
2015-09-30 11:50:55 +0800

    建立按鈕的圖案與行為框架,
    1.drawable放上兩張圖片,play and stop icon
    2.拉一顆按鈕,先取得物件實體,再建立點擊事件的框架

--- MyMusicPlayer/app/src/main/java/com/example/henryho/mymusicplayer/MainActivity.java
+++ MyMusicPlayer/app/src/main/java/com/example/henryho/mymusicplayer/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.henryho.mymusicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_playOrStop;
+    private boolean isMusicPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setListeners();
     }
 
+    private void initViews() {
+        btn_playOrStop = (Button) findViewById(R.id.btn_playOrStop);
+        btn_playOrStop.setBackgroundResource(R.drawable.button_play_icon);
+    }
+
+    private void setListeners() {
+        btn_playOrStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isMusicPlaying) {
+                    isMusicPlaying = true;
+                    playAudio();
+                    btn_playOrStop.setBackgroundResource(R.drawable.button_stop_icon);
+                } else if (isMusicPlaying) {
+                    isMusicPlaying = false;
+                    stopAudio();
+                    btn_playOrStop.setBackgroundResource(R.drawable.button_play_icon);
+                }
+            }
+        });
+    }
+
+    private void playAudio() {
+    }
+
+    private void stopAudio() {
+    }
 }
