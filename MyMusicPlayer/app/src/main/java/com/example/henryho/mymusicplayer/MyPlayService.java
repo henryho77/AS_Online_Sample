a11242e619ae87e1c6bc10f268566d3e8d1aea93
henryho77 <squirrelho@gmail.com>
2015-09-30 15:53:08 +0800

    在Service建立MediaPlayer與其事件處理
    mediaPlayer.setOnBufferingUpdateListener(this);
    mediaPlayer.setOnCompletionListener(this);
    mediaPlayer.setOnErrorListener(this);
    mediaPlayer.setOnInfoListener(this);
    mediaPlayer.setOnPreparedListener(this);
    mediaPlayer.setOnSeekCompleteListener(this);

--- MyMusicPlayer/app/src/main/java/com/example/henryho/mymusicplayer/MyPlayService.java
+++ MyMusicPlayer/app/src/main/java/com/example/henryho/mymusicplayer/MyPlayService.java
@@ -2,12 +2,74 @@ package com.example.henryho.mymusicplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
-public class MyPlayService extends Service {
+public class MyPlayService extends Service implements MediaPlayer.OnBufferingUpdateListener,
+                                                      MediaPlayer.OnCompletionListener,
+                                                      MediaPlayer.OnErrorListener,
+                                                      MediaPlayer.OnInfoListener,
+                                                      MediaPlayer.OnPreparedListener,
+                                                      MediaPlayer.OnSeekCompleteListener {
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+        mediaPlayer.setOnInfoListener(this);
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnSeekCompleteListener(this);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    /*===============================================================================*/
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public void onSeekComplete(MediaPlayer mp) {
+
+    }
+    /*===============================================================================*/
 }
