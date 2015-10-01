89fac4583d84bb9a4f3af67edb502365849bc946
henryho77 <squirrelho@gmail.com>
2015-10-01 14:33:48 +0800

    建立推播功能
    1.開頭處新增NOTIFICATION_ID
    2.onCreate()建立推播
    3.onDestroy()取消推播

--- MyMusicPlayer/app/src/main/java/com/example/henryho/mymusicplayer/MyPlayService.java
+++ MyMusicPlayer/app/src/main/java/com/example/henryho/mymusicplayer/MyPlayService.java
@@ -1,5 +1,8 @@
 package com.example.henryho.mymusicplayer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -17,6 +20,8 @@ public class MyPlayService extends Service implements MediaPlayer.OnBufferingUpd
 
     MediaPlayer mediaPlayer = new MediaPlayer();
     private String sntAudioLink;
+    private static final int NOTIFICATION_ID = 1;//2.Set up the notification ID
+
 
     @Override
     public void onCreate() {
@@ -34,6 +39,10 @@ public class MyPlayService extends Service implements MediaPlayer.OnBufferingUpd
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //return super.onStartCommand(intent, flags, startId);
+
+
+        /*2.Insert notification start */
+        initNotification();
 
         sntAudioLink = intent.getExtras().getString("sentAudioLink");//1.
         mediaPlayer.reset();//1.
@@ -63,6 +72,9 @@ public class MyPlayService extends Service implements MediaPlayer.OnBufferingUpd
             }
             mediaPlayer.release();
         }
+
+        //2.Cancel the notification
+        cancelNotification();
     }
 
     @Override
@@ -130,5 +142,44 @@ public class MyPlayService extends Service implements MediaPlayer.OnBufferingUpd
         if (mediaPlayer.isPlaying()) {
             mediaPlayer.stop();
         }
+    }
+
+
+    //2.Create notification
+    private void initNotification() {
+        NotificationManager notiMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Notification.Builder notiBuilder = new Notification.Builder(this);
+        notiBuilder.setSmallIcon(R.mipmap.ic_launcher);//推播的圖案
+        notiBuilder.setContentTitle("Music In Service App Tutorial");//推播下拉時所顯示的標題
+        notiBuilder.setContentText("Listen To Music While Performing Other Tasks");//推播下拉時所顯示的內文
+        notiBuilder.setTicker("Tutorial: Music In Service");//推播剛跳出時,會有一小段時間可以顯示即時的文字,就是用setTicker做的
+        notiBuilder.setWhen(System.currentTimeMillis());//推播下拉時,右邊會顯示該推播的時間點,就是用setWhen做的
+        notiBuilder.setAutoCancel(true);
+
+        Intent notiIntent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(getApplicationContext(), 0, notiIntent, 0);
+        notiBuilder.setContentIntent(pIntent);
+
+        Notification noti = notiBuilder.build();
+        notiMgr.notify(NOTIFICATION_ID, noti);
+
+        /*早期的寫法*/
+        //int icon = R.drawable.ic_launcher;
+        //CharSequence tickerText = "Tutorial: Music In Service";
+        //long when = System.currentTimeMillis();
+        //Notification noti = new Notification(icon, tickerText, when);
+        //noti.flags = Notification.FLAG_ONGOING_EVENT;
+        //Context context = getApplicationContext();
+        //CharSequence contentTitle = "Music In Service App Tutorial";
+        //CharSequence contentText = "Listen To Music While Performing Other Tasks";
+        //Intent notiIntent = new Intent(this, MyActivity.class);
+        //PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notiIntent, 0);
+    }
+
+    //2.Cancel notification
+    private void cancelNotification() {
+        NotificationManager notiMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notiMgr.cancel(NOTIFICATION_ID);
     }
 }
