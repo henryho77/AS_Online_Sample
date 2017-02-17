f8ba4271b7f6b8110e4354b7a9693c943ca2e126
Henry Ho <squirrelho@gmail.com>
2017-02-17 12:34:42 +0800

    FbSample

---
+++ FbSample/app/src/main/java/com/example/fbsample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fbsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button btnFbLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFbLogin = (Button) findViewById(R.id.btnFbLogin);
+        btnFbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
